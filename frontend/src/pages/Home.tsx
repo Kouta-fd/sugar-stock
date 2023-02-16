fa63bd9d19f0fcbddea45bead70fc790192f0675
Kouta-fd <kotafd0124@icloud.com>
2023-02-16 13:37:38 +0900

    feat:home modified

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -4,18 +4,22 @@ const HomePage = () => {
     <div className="mt-16">
       <h1 className="text-5xl text-center">糖</h1>
       <div className="text-center mt-24">
-        <Link
-          className="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          to={`/user-login/`}
-        >
-          ログイン
-        </Link>
-        <Link
-          className="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-          to={`/barcorde-scanner`}
-        >
-          ログインしないで使う
-        </Link>
+        <div>
+          <Link
+            className="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            to={`/user-login/`}
+          >
+            ログイン
+          </Link>
+        </div>
+        <div className="mt-5">
+          <Link
+            className="bg-orange-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            to={`/barcorde-scanner`}
+          >
+            ログインしないで使う
+          </Link>
+        </div>
       </div>
     </div>
   );
